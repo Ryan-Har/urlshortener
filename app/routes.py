133d98eeb0d53466e0a99372adee1cca6b317202
Ryan Harris <ryan@harrisit.co.uk>
2020-08-04 06:47:09 +0100

    final

--- app/routes.py
+++ app/routes.py
@@ -7,10 +7,6 @@ import re
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/index', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
-
-@app.route('/makeurl', methods=['POST', 'GET'])
-def sendurl():
     urlRegex = re.compile(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)')
     if request.method == "POST":
         input = str(request.form["input"])
@@ -19,7 +15,9 @@ def sendurl():
             dbfunction.addUrl(generatedId, input)
             return render_template('shortenedurl.html', url = generatedId)
         else:
-            return render_template('index.html', error = input, msg = ' is not a valid url')
+            return render_template('index.html', error = input, msg = ' is not a valid url')    
+    else:
+        return render_template('index.html')
 
 @app.route('/<shorturl>')
 def forward(shorturl):
